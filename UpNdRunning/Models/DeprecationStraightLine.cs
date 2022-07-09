642d117312283061d9a1cb50f097fc1b0daa1f0d
Zotigh <lzotigh1@cnm.edu>
2022-07-09 15:14:44 -0600

    References are fixed however the code will not run the desired calculator page.

--- UpNdRunning/Models/DeprecationStraightLine.cs
+++ UpNdRunning/Models/DeprecationStraightLine.cs
@@ -6,7 +6,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace UpNdRunning_.Models
+namespace UpNdRunning.Models
 {
     public class DeprecationStraightLine
     {
--- UpNdRunning/Models/SuccessfullyCalculatedResults.cs
+++ UpNdRunning/Models/SuccessfullyCalculatedResults.cs
@@ -7,6 +7,7 @@
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace UpNdRunning.Models
 {
